a192541603af76cc92d5c5694392e5d65ae3b72f
xiaoniuzi <dingxudong1@huawei.com>
2026-03-11 18:15:57 +0800

    更新日志存放的目录

--- scripts/submit-jobs.sh
+++ scripts/submit-jobs.sh
@@ -12,11 +12,10 @@ timeout=${timeout-86400}
 poll_interval=${poll_interval-10}
 extra=${extra-"os_mount=initramfs"}
 
-testcase_dir="${WORKSPACE}/tmp/testcase_logs"
+testcase_dir="${WORKSPACE}/testcase_logs"
 if [ ! -d "${testcase_dir}" ];then
   sudo mkdir -p ${testcase_dir}
 fi
-testcase_logs_dir=$(sudo mktemp -d -p "${testcase_dir}" "XXXXXX")
 echo "当前执行用户：$(whoami)"
 echo "the testcase is ${job_yaml}, and the logs dir is ${testcase_logs_dir}"
 # 获取脚本所在目录的绝对路径
@@ -37,14 +36,4 @@ python3 -u src/submit_wait_job.py \
 --poll_interval ${poll_interval} \
 --timeout ${timeout} \
 --extra "${extra}" \
---logs_dir ${testcase_logs_dir}
-
-#放到workspace下面，方便归档到文件服务器
-if [ -n "$(ls -A "${testcase_logs_dir}")" ];then
-  mkdir -p "${WORKSPACE}"/testcase_logs/
-  cp -a "${testcase_logs_dir}"/. "${WORKSPACE}"/testcase_logs/
-fi
-
-if [ -d "${testcase_logs_dir}" ];then
-  sudo rm -rf "${testcase_logs_dir}"
-fi
+--logs_dir ${testcase_dir}
